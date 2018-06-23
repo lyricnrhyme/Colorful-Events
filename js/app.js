25486ae146d4f0f4157a9d86b73e7a33c723923f
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-06-22 18:13:10 -1000

    rando colo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
+
+var button = document.createElement("button");
+button.id = "button";
+button.innerHTML = "Click me!";
+document.body.appendChild(button);
+
+button.addEventListener("click", printRandomColor);
+
+
+function printRandomColor() {
+  document.body.style.backgroundColor = getRandomColor();
+}
